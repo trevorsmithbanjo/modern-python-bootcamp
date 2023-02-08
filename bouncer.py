0c9634d557530060d1398c646e7ef8f57ff02b8e
Trevor Smith <trevorsmithbanjo@gmail.com>
2023-02-08 16:59:23 -0600

    Adds bouncer.py

---
+++ bouncer.py
@@ -0,0 +1,12 @@
+age = input("how old are you: ")
+
+if age:
+    age = int(age)
+    if age >= 21:
+        print("You are good to enter and can drink!")
+    elif age >= 18:
+        print("You can enter, but need a wristband")
+    else:
+        print("You can't come in!")
+else:
+    print("Please enter an age!")
