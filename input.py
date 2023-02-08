4e0ccbe8a138285a8b12d82e9e870040a6ed9267
Trevor Smith <trevorsmithbanjo@gmail.com>
2023-02-08 16:26:49 -0600

    Adds input.py

---
+++ input.py
@@ -0,0 +1,12 @@
+# print("What's your favorite color?")
+# data = input()
+# print("You said " + data)
+
+print("What is your name?")
+name = input()
+if name == "Arya Stark":
+    print("Valar Morghulis")
+elif name == "Jon Snow":
+    print("You know nothing")
+else: 
+    print("Carry on " + name)
